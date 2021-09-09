dd64f4bac5082eb138d83353dbb25cec6f476d72
Jose Gamboa <jose.gamboa@mail.escuelaing.edu.co>
2021-09-08 21:54:46 -0500

    Correcta introduccion de la libreria JUnit

--- src/test/java/hangman/model/GameScoreTest.java
+++ src/test/java/hangman/model/GameScoreTest.java
@@ -1,16 +1,13 @@
 package hangman.model;
 
-import hangman.model;
-
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class GameScoreTest {
 
 	@Test
 	public void test() {
-		Assert.assertEquals(true,true);
+		Assert.assertEquals(0, 0);
 	}
 
 }

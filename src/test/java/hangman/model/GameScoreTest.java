12bb56ebdbaae9c63ede412fb306976b4b3489eb
Jose Gamboa <jose.gamboa@mail.escuelaing.edu.co>
2021-09-08 21:49:14 -0500

    Copia de seguridad

---
+++ src/test/java/hangman/model/GameScoreTest.java
@@ -0,0 +1,16 @@
+package hangman.model;
+
+import hangman.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameScoreTest {
+
+	@Test
+	public void test() {
+		Assert.assertEquals(true,true);
+	}
+
+}

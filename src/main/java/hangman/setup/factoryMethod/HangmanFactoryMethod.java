e7c99242c925f85c16a4aea0516558947213d4fd
Jose Gamboa <jose.gamboa@mail.escuelaing.edu.co>
2021-09-08 23:22:48 -0500

    Inversion de dependencias

--- src/main/java/hangman/setup/factoryMethod/HangmanFactoryMethod.java
+++ src/main/java/hangman/setup/factoryMethod/HangmanFactoryMethod.java
@@ -1,11 +1,23 @@
 package hangman.setup.factoryMethod;
 
+import hangman.model.GameScore;
 import hangman.model.Language;
+import hangman.model.OriginalScore;
 import hangman.model.dictionary.HangmanDictionary;
 import hangman.view.HangmanPanel;
+import static com.google.inject.Guice.createInjector;
 
-abstract public class HangmanFactoryMethod {
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
+
+abstract public class HangmanFactoryMethod extends AbstractModule{
     abstract public Language createLanguage();
     abstract public HangmanDictionary createDictionary();
     abstract public HangmanPanel createHangmanPanel();
+    
+    @Override
+    protected void configure() {
+    	
+    	bind(GameScore.class).to(OriginalScore.class);
+    }
 }
